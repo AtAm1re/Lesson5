e7a4beefed698c499f869a140d6e383c2e7dfcd3
AtAm1re <168696141+AtAm1re@users.noreply.github.com>
2024-05-12 21:27:33 +0300

    Add files via upload

---
+++ Module1/homework4.py
@@ -0,0 +1,6 @@
+immutable_var = (1, 'limon', True)
+print(immutable_var)
+# immutable_var[0] = 100, кортеж изменить нельзя, так как является неизменяемым типом данных
+mutable_list = [2, 'magnit', False]
+mutable_list[1] = 'London'
+print(mutable_list)
---
+++ Module1/homework5.py
@@ -0,0 +1,13 @@
+my_list = (['Апельсин', 'Яблоко', 'Авокадо', 'Персик', 'Абрикос'])
+print(my_list)
+print(my_list[0] + ' ' + my_list[4])
+print(my_list[2:5])
+my_list[2] = 'Виноград'
+print(my_list)
+
+my_dict = {'Апельсин': 'Orange', 'Яблоко': 'Apple', 'Авокадо': 'Avocado', 'Персик': 'Persik'}
+print(my_dict)
+print(my_dict['Апельсин'])
+my_dict['Персик'] = 'Peach'
+my_dict['Виноград'] = 'Grape'
+print(my_dict)

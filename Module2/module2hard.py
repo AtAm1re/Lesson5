0a21ece242ecf8bfedcea97ba61ed2596f71f832
AtAm1re <168696141+AtAm1re@users.noreply.github.com>
2024-05-21 17:35:32 +0300

    Update module2hard.py

--- Module2/module2hard.py
+++ Module2/module2hard.py
@@ -7,10 +7,9 @@ result = ''
 
 for num_1 in range(1, 21):
     for num_2 in range(1, 21):
-        if num_1 != num_2:
+        if num_1 < num_2:
             if number_left % (num_1 + num_2) == 0:
-                if num_1 < num_2:
-                    result += str(num_1) + str(num_2)
+                result += str(num_1) + str(num_2)
 
 print(result)
 

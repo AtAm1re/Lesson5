7ad01f0d2f215799d40057374ee4a756e2276b39
AtAm1re <168696141+AtAm1re@users.noreply.github.com>
2024-06-10 13:11:23 +0300

    Add files via upload

---
+++ module4/homework4_2.py
@@ -0,0 +1,10 @@
+def test_function():
+    def inner_function():
+        return 'Я в области видимости функции test_function'
+
+    return inner_function()
+
+
+print(test_function())
+print(inner_function())
+

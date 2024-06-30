ba13a2666b8e02f179e3ed8f320a0e5aa7dce518
AtAm1re <168696141+AtAm1re@users.noreply.github.com>
2024-06-30 16:07:39 +0300

    Update homework5_3.py

--- module5/homework5_3.py
+++ module5/homework5_3.py
@@ -1,10 +1,12 @@
 class Building:
-    def __init__(self):
-        self.numberOfFloors = int
-        self.buildingType = str
+    def __init__(self, numberOfFloors, buildingType):
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
 
-    def __eq__(self):
-        print(self.numberOfFloors == self.buildingType)
+    def __eq__(self, other):
+        return self.numberOfFloors == other.numberOfFloors
 
-a = Building()
-a.__eq__()
+
+a = Building(12, 'Apartments')
+b = Building(16, 'Apartments')
+print(a == b)
